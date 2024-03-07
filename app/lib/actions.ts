b23d5dee0857c021cdfcf2f71e404b953fcafbfa
kimjinsub <friday@softworks.co.kr>
2024-03-07 17:15:24 +0900

    form validation - server side

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -43,7 +43,8 @@ export async function createInvoice(prevState: State, formData: FormData) {
       message: 'Missing Fields. Failed to Create Invoice.',
     };
   }
-
+  // Prepare data for insertion into the database
+  const {customerId, amount, status} = validatedFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
@@ -65,13 +66,21 @@ export async function createInvoice(prevState: State, formData: FormData) {
 // Use Zod to update the expected types
 const UpdateInvoice = FormSchema.omit({id: true, date: true});
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
-    status: formData.get('status'),
+    status: formData.get('status')
   });
-
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.'
+    };
+  }
+  // Prepare data for insertion into the database
+  const {customerId, amount, status} = validatedFields.data;
   const amountInCents = amount * 100;
 
   try {
